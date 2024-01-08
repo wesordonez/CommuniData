083c45136b9b82eb7c07996e56bf2c03b85756f4
Wesley Ordoñez <wesordonez1@gmail.com>
2024-01-08 11:01:00 -0600

    added first test view for home app.

--- data_visualizer/home/views.py
+++ data_visualizer/home/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse('<h1>Hello, world. You are at the home index.</h1>')
 
 # Create your views here.
