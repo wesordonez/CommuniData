6fc61c41d106a55abfe05dd020c1f4a4fbb05d46
Wesley Ordoñez <wesordonez1@gmail.com>
2024-02-12 20:47:48 -0600

    minichanges

--- data_visualizer/home/urls.py
+++ data_visualizer/home/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('contact/', views.ContactFromView.as_view(), name='contact'),
+    path('contact/', views.ContactFromView, name='contact'),
 ]
