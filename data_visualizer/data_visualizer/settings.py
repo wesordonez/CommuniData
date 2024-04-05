c64287235663851ffe04b1538b26a064f8457ab9
Wesley Ordoñez <wesordonez1@gmail.com>
2024-04-04 22:34:12 -0500

    initial commit, installed django rest framework

--- data_visualizer/data_visualizer/settings.py
+++ data_visualizer/data_visualizer/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'home.apps.HomeConfig',
     'dashboard.apps.DashboardConfig',
     'session.apps.SessionConfig',
