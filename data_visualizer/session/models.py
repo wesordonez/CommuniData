1b1a314c5cf7d7bc5ffd1695cdee27b64e1c710b
Wesley Ordoñez <102090143+wesordonez@users.noreply.github.com>
2024-04-09 21:59:12 -0500

    Session data models (#45)
    * Add Buildings model to manage building data
    
    * Add Contacts and Business models
    
    * Add Clients model to manage client data
    
    * Add Sessions model to data_visualizer session module

--- data_visualizer/session/models.py
+++ data_visualizer/session/models.py
@@ -24,6 +24,14 @@ SPECIALTY_CHOICES = [
     ('5', 'Technology Specialist'),
     ('6', 'Other')
 ]
+PIN_REGEX = RegexValidator(
+    regex=r'^\d{2}-\d{2}-\d{3}-\d{3}-\d{4}$',
+    message="PIN must be in the format 00-00-000-000-0000."
+)
+PROPERTY_CLASS_REGEX = RegexValidator(
+    regex=r'^\d{1}-\d{2}$',
+    message="Property class must be in the format 0-00."
+)
 
 
 class Consultant(models.Model):
@@ -103,3 +111,245 @@ class BusinessInitiativeProgram(models.Model):
     class Meta:
         db_table = 'bips'
         
+        
+class Buildings(models.Model):
+    """Building data model.
+
+    Args:
+        models (module): The Django models module.
+        buildingId (int, optional): The ID of the building. Defaults to models.AutoField(primary_key=True).
+
+    Attributes:
+        pin: CharField (Personal Identification Number, no dashes)
+        address: CharField (Full address)
+        address_number: CharField or IntegerField (Address number)
+        address_street: CharField (Street name of address)
+        zip_code: CharField or IntegerField (ZIP code)
+        chicago_owned_property: CharField (Boolean-like, might need clarification)
+        property_class: CharField or IntegerField (Class code)
+        property_description: TextField (Description of the property)
+        bill_2020: DecimalField (Property tax bill amount for 2020)
+        bill_2021: DecimalField (Property tax bill amount for 2021)
+        assessment_2020: DecimalField (Property tax assessment for 2020)
+        assessment_2021: DecimalField (Property tax assessment for 2021)
+        units: IntegerField (Number of units)
+        area_sq_ft: DecimalField (Area in square feet)
+        lot_size_sf: DecimalField (Lot size in square feet)
+        property_tax_year: IntegerField (The tax year for the property)
+        taxpayer_name: CharField (Name of the taxpayer)
+        taxpayer_address: CharField (Address of the taxpayer)
+        taxpayer_city_state_zip: CharField (City, state, and ZIP of the taxpayer)
+        time_last_checked: DateTimeField (Last time the data was checked)
+        
+    """
+
+    building_id = models.AutoField(primary_key=True)
+    pin = models.CharField(validators=[PIN_REGEX], max_length=20)
+    address = models.CharField(max_length=MAX_CHAR_LENGTH)
+    address_number = models.IntegerField()
+    address_street = models.CharField(max_length=MAX_CHAR_LENGTH)
+    zip_code = models.IntegerField()
+    chicago_owned_property = models.BooleanField()
+    property_class = models.CharField(validators=[PROPERTY_CLASS_REGEX], max_length=5)
+    property_description = models.TextField()
+    tax_bill_2020 = models.DecimalField(max_digits=10, decimal_places=2)
+    tax_bill_2021 = models.DecimalField(max_digits=10, decimal_places=2)
+    assessment_2020 = models.DecimalField(max_digits=10, decimal_places=2)
+    assessment_2021 = models.DecimalField(max_digits=10, decimal_places=2)
+    units = models.IntegerField()
+    area_sq_ft = models.DecimalField(max_digits=10, decimal_places=2)
+    lot_size_sf = models.DecimalField(max_digits=10, decimal_places=2)
+    property_tax_year = models.IntegerField()
+    taxpayer_name = models.CharField(max_length=MAX_CHAR_LENGTH)
+    taxpayer_address = models.CharField(max_length=MAX_CHAR_LENGTH)
+    taxpayer_city_state_zip = models.CharField(max_length=MAX_CHAR_LENGTH)
+    time_last_checked = models.DateTimeField()
+    
+
+    class Meta:
+        db_table = 'buildings'
+        constraints = [
+            UniqueConstraint(
+                fields=['pin', 'address_number'],
+                name='unique_building',
+            ),
+        ]
+        
+        
+class Contacts(models.Model):
+    """Contact data model.
+    
+    Args:
+        models (module): The Django models module.
+        contactId (int, optional): The ID of the contact. Defaults to models.AutoField(primary_key=True).
+
+    Attributes:
+        contactId (int): The ID of the contact and Primary Key of the table.
+        first_name (str): The first name of the contact.
+        last_name (str): The last name of the contact.
+        email (str): The email address of the contact.
+        phone (str): The phone number of the contact.
+        business_id (int): Foreign Key to the company the contact is associated with.
+        business_role (str): The role of the contact in the business.
+        alt_phone (str): An alternate phone number for the contact.
+        address (str): The mailing address of the contact.
+        date_of_birth (Date): The date of birth of the contact.
+        gender (str): The gender of the contact.
+        ethnicity (str): The ethnicity of the contact.
+        nationality (str): The nationality of the contact.
+        language (str): The language spoken by the contact.
+        registration_date (Date): The date the contact was registered.
+        notes (str): Any notes about the contact.
+        created_at (DateTime): The date and time the contact was created.
+        updated_at (DateTime): The date and time the contact was last updated.
+        
+    """
+    
+    contact_id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=MAX_CHAR_LENGTH)
+    last_name = models.CharField(max_length=MAX_CHAR_LENGTH)
+    email = models.EmailField(max_length=MAX_CHAR_LENGTH)
+    phone = models.CharField(validators=[PHONE_REGEX], max_length=15)
+    business_id = models.ForeignKey('Business', on_delete=models.CASCADE)
+    business_role = models.CharField(max_length=MAX_CHAR_LENGTH)
+    alt_phone = models.CharField(validators=[PHONE_REGEX], max_length=15)
+    address = models.CharField(max_length=MAX_CHAR_LENGTH)
+    date_of_birth = models.DateField
+    gender = models.CharField(max_length=MAX_CHAR_LENGTH)
+    ethnicity = models.CharField(max_length=MAX_CHAR_LENGTH)
+    nationality = models.CharField(max_length=MAX_CHAR_LENGTH)
+    language = models.CharField(max_length=MAX_CHAR_LENGTH)
+    registration_date = models.DateField
+    notes = models.TextField
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        db_table = 'contacts'
+        
+        
+class Business(models.Model):
+    """Business data model.
+
+    Args:
+        models (module): The Django models module.
+        businessId (int, optional): The ID of the business. Defaults to models.AutoField(primary_key=True).
+        
+    Attributes:
+        business_id (int): The ID of the business and Primary Key of the table.
+        name (str): The name of the business.
+        dba (str): The "doing business as" name of the business.
+        business_description (str): The description of the business.
+        address (str): The address of the business.
+        phone (str): The phone number of the business.
+        email (str): The email address of the business.
+        website (str): The website of the business.
+        industry (str): The industry the business is in.
+        naics_code (str): The NAICS code of the business.
+        date_established (Date): The date the business was established.
+        legal_structure (str): The legal structure of the business.
+        ein (str): The Employer Identification Number of the business.
+        licenses (str): The licenses the business has.
+        contact_id (int): Foreign Key of the contact person for the business.
+        num_employees (int): The number of employees at the business.
+        status (str): The status of the business.
+        notes (str): Any notes about the business.
+        created_at (DateTime): The date and time the business was created.
+        updated_at (DateTime): The date and time the business was last updated.
+    
+    """
+    
+    business_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=MAX_CHAR_LENGTH)
+    dba = models.CharField(max_length=MAX_CHAR_LENGTH)
+    business_description = models.TextField
+    address = models.CharField(max_length=MAX_CHAR_LENGTH)
+    phone = models.CharField(validators=[PHONE_REGEX], max_length=15)
+    email = models.EmailField(max_length=MAX_CHAR_LENGTH)
+    website = models.URLField
+    industry = models.CharField(max_length=MAX_CHAR_LENGTH)
+    naics_code = models.CharField(max_length=MAX_CHAR_LENGTH)
+    date_established = models.DateField
+    legal_structure = models.CharField(max_length=MAX_CHAR_LENGTH)
+    ein = models.CharField(max_length=MAX_CHAR_LENGTH)
+    licenses = models.CharField(max_length=MAX_CHAR_LENGTH)
+    contact_id = models.ForeignKey('Contacts', on_delete=models.CASCADE)
+    num_employees = models.IntegerField
+    status = models.CharField(max_length=MAX_CHAR_LENGTH)
+    notes = models.TextField
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        db_table = 'businesses'
+        
+        
+class Clients(models.Model):
+    """Client data model.
+
+    Args:
+        models (module): The Django models module.
+        clientId (int, optional): The ID of the client. Defaults to models.AutoField(primary_key=True).
+        
+    Attributes:
+        client_id (int): The ID of the client and Primary Key of the table.
+        business_id (int): Foreign Key to the business the client is associated with.
+        contact_id (int): Foreign Key to the contact person for the client.
+        consultant_id (int): Foreign Key to the consultant the client is associated with.
+        status (str): The status of the client.
+        notes (str): Any notes about the client.
+        
+    """
+    
+    client_id = models.AutoField(primary_key=True)
+    business_id = models.ForeignKey('Business', on_delete=models.CASCADE)
+    contact_id = models.ForeignKey('Contacts', on_delete=models.CASCADE)
+    consultant_id = models.ForeignKey('Consultant', on_delete=models.CASCADE)
+    status = models.CharField(max_length=MAX_CHAR_LENGTH)
+    notes = models.TextField
+    
+    class Meta:
+        db_table = 'clients'
+        constraints = [
+            UniqueConstraint(
+                fields=['business_id', 'contact_id'],
+                name='unique_client',
+            ),
+        ]
+        
+        
+class Sessions(models.Model):
+    """Session data model.
+
+    Args:
+        models (module): The Django models module.
+        sessionId (int, optional): The ID of the session. Defaults to models.AutoField(primary_key=True).
+        
+    Attributes:
+        session_id (int): The ID of the session and Primary Key of the table.
+        client_id (int): Foreign Key to the client the session is associated with.
+        consultant_id (int): Foreign Key to the consultant the session is associated with.
+        date (Date): The date of the session.
+        duration (Time): The duration of the session (HH:MM).
+        notes (str): Any notes about the session.
+        status (str): The status of the session.
+        follow_up (date): The follow-up date of the session.
+        created_at (DateTime): The date and time the session was created.
+        updated_at (DateTime): The date and time the session was last updated.        
+        
+    """
+    
+    session_id = models.AutoField(primary_key=True)
+    client_id = models.ForeignKey('Clients', on_delete=models.CASCADE)
+    consultant_id = models.ForeignKey('Consultant', on_delete=models.CASCADE)
+    date = models.DateField
+    duration = models.TimeField
+    notes = models.TextField
+    status = models.CharField(max_length=MAX_CHAR_LENGTH)
+    follow_up = models.DateField
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        db_table = 'advising_sessions'
+    
