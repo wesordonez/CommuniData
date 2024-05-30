fb34a70b4bea00601610c0830aaa07308aa81455
Wesley Ordoñez <wesordonez1@gmail.com>
2024-05-30 18:28:56 -0500

    added data import script

---
+++ app/dashboard/management/commands/data_import.py
@@ -0,0 +1,64 @@
+""" This script will import the data from the business_licenses.csv file into the database. Will not be used during production.
+"""
+
+from django.core.management.base import BaseCommand
+from dashboard.models import BusinessLicenses
+import pandas as pd
+
+
+file_path = '/Data_Visualizer/business_licenses.csv'
+
+
+class Command(BaseCommand):
+    help = 'Imports data from business_licenses.csv into the database.'
+    
+    def handle(self, *args, **options):
+        
+        df = pd.read_csv(file_path)
+        df.columns = df.columns.str.lower()
+        for column in df.columns:
+            df.rename(columns={column: column.replace(" ", "_")}, inplace=True)
+        date_columns = ['application_created_date', 'application_requirements_complete', 'payment_date', 'license_term_start_date', 'license_term_expiration_date', 'license_approved_for_issuance', 'date_issued', 'license_status_change_date']
+        for column in date_columns:
+            df[column] = pd.to_datetime(df[column], format='%m/%d/%Y', errors='coerce').dt.date
+        df = df.where(pd.notnull(df), None)
+        
+        for index, row in df.iterrows():
+            BusinessLicenses.objects.create(
+                business_id=row['id'],
+                license_id=row['license_id'],
+                account_number=row['account_number'],
+                site_number=row['site_number'],
+                legal_name=row['legal_name'],
+                doing_business_as_name=row['doing_business_as_name'],
+                address=row['address'],
+                city=row['city'],
+                state=row['state'],
+                zip_code=row['zip_code'],
+                ward=row['ward'],
+                precinct=row['precinct'],
+                ward_precinct=row['ward_precinct'],
+                police_district=row['police_district'],
+                license_code=row['license_code'],
+                license_description=row['license_description'],
+                business_activity_id=row['business_activity_id'],
+                business_activity=row['business_activity'],
+                license_number=row['license_number'],
+                application_type=row['application_type'],
+                application_created_date=row['application_created_date'],
+                application_requirements_complete=row['application_requirements_complete'],
+                payment_date=row['payment_date'],
+                conditional_approval=row['conditional_approval'],
+                license_term_start_date=row['license_term_start_date'],
+                license_term_expiration_date=row['license_term_expiration_date'],
+                license_approved_for_issuance=row['license_approved_for_issuance'],
+                date_issued=row['date_issued'],
+                license_status=row['license_status'],
+                license_status_change_date=row['license_status_change_date'],
+                ssa=row['ssa'],
+                latitude=row['latitude'],
+                longitude=row['longitude'],
+                location=row['location'],
+            )
+        self.stdout.write(self.style.SUCCESS('Data imported successfully!'))
+    
