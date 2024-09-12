ec2d6afcb3aed1eec06343334427e03fdfbe8085
Wesley Ordoñez <wesordonez1@gmail.com>
2024-09-11 20:08:02 -0500

    changed django settings module in wsgi.py

--- app/data_visualizer/wsgi.py
+++ app/data_visualizer/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'data_visualizer.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.data_visualizer.settings')
 
 application = get_wsgi_application()
