69a6a471954717fe0ef603ddbf858f37733c2a0a
Wesley Ordoñez <wesordonez1@gmail.com>
2024-09-17 04:33:37 -0500

    testing api calls, not working yet

--- app/dashboard/management/commands/data_import.py
+++ app/dashboard/management/commands/data_import.py
@@ -4,6 +4,32 @@
 from django.core.management.base import BaseCommand
 from dashboard.models import BusinessLicenses
 import pandas as pd
+import requests
+from io import StringIO
+import os
+from dotenv import load_dotenv
+dotenv_path = os.path.join(os.path.dirname(__file__), '../.env')
+load_dotenv(dotenv_path)
+
+# API URL for fetching the data from Chicago Data Portal
+url = "https://data.cityofchicago.org/resource/4mfv-i89s.csv?$query=SELECT%0A%20%20%60id%60%2C%0A%20%20%60license_id%60%2C%0A%20%20%60account_number%60%2C%0A%20%20%60site_number%60%2C%0A%20%20%60legal_name%60%2C%0A%20%20%60doing_business_as_name%60%2C%0A%20%20%60address%60%2C%0A%20%20%60city%60%2C%0A%20%20%60state%60%2C%0A%20%20%60zip_code%60%2C%0A%20%20%60ward%60%2C%0A%20%20%60precinct%60%2C%0A%20%20%60ward_precinct%60%2C%0A%20%20%60police_district%60%2C%0A%20%20%60license_code%60%2C%0A%20%20%60license_description%60%2C%0A%20%20%60business_activity_id%60%2C%0A%20%20%60business_activity%60%2C%0A%20%20%60license_number%60%2C%0A%20%20%60application_type%60%2C%0A%20%20%60application_created_date%60%2C%0A%20%20%60application_requirements_complete%60%2C%0A%20%20%60payment_date%60%2C%0A%20%20%60conditional_approval%60%2C%0A%20%20%60license_start_date%60%2C%0A%20%20%60expiration_date%60%2C%0A%20%20%60license_approved_for_issuance%60%2C%0A%20%20%60date_issued%60%2C%0A%20%20%60license_status%60%2C%0A%20%20%60license_status_change_date%60%2C%0A%20%20%60ssa%60%2C%0A%20%20%60latitude%60%2C%0A%20%20%60longitude%60%2C%0A%20%20%60location%60%0AWHERE%0A%20%20(%60ward%60%20IN%20(%2226%22))%0A%20%20AND%20(%60date_issued%60%0A%20%20%20%20%20%20%20%20%20BETWEEN%20%222024-09-01T04%3A03%3A33%22%20%3A%3A%20floating_timestamp%0A%20%20%20%20%20%20%20%20%20AND%20%222024-09-17T04%3A03%3A33%22%20%3A%3A%20floating_timestamp)"
+
+app_token = "CDP_APP_TOKEN"
+
+headers = {
+    "X-App-Token": app_token
+}
+
+response = requests.get(url, headers=headers)
+
+# Check if request was successful
+if response.status_code == 200:
+    api_data = StrigIO(response.text)
+    df = pd.read_csv(api_data)
+else:
+    print("Failed to fetch data from the API. Status code: ", response.status_code)
+
+# Path to the csv file
 
 file_path = '../business_licenses_test.csv'
 
@@ -21,7 +47,7 @@ class Command(BaseCommand):
         
         # Import and clean the data
         
-        df = read_csv_auto(file_path)
+        # df = read_csv_auto(file_path)
         df.columns = df.columns.str.lower()
         
         for column in df.columns:
