a1ebf5f3c07400577fa36b81a5a641c33ca579c8
Wesley Ordoñez <wesordonez1@gmail.com>
2024-10-21 17:29:12 -0500

    changed names of header nav links, added scroll to section functionality

--- app/home/static/home/js/script.js
+++ app/home/static/home/js/script.js
@@ -46,3 +46,24 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
+// Smooth scroll to next section
+document.addEventListener('DOMContentLoaded', function() {
+  const links = document.querySelectorAll('a[href^="#"]');
+
+  for (const link of links) {
+      link.addEventListener('click', function(event) {
+          event.preventDefault();
+
+          const targetId = this.getAttribute('href').substring(1);
+          const targetElement = document.getElementById(targetId);
+
+          if (targetElement) {
+              window.scrollTo({
+                  top: targetElement.offsetTop,
+                  behavior: 'smooth'
+              });
+          }
+      });
+  }
+});
